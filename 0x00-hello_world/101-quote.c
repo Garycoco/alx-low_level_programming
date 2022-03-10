9e520aa3c5d67e245d3a9ec7c65f21bb31824d48
Gary <76396923+Garycoco@users.nonreply.github.com>
2022-03-10 10:14:02 -0800

    Intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Prints out the the quote
+ * Return: 1 if success
+ */
+
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpa 2015-10-19\n");
+	return (1);
+}
