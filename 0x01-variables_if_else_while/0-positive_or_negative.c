e54bd840788d14fa0b0f83fe939055b08e346c69
Gary Masinga <76396923+Garycoco@users.nonreply.github.com>
2022-03-11 13:54:27 +0000

    Positive and negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,7 @@
 #include <time.h>
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 
@@ -15,11 +16,11 @@ int main(void)
 	if (n < 0)
 	{
 		printf("%d is negative\n", n);
-	} 
+	}
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
-	} 
+	}
 	else
 	{
 		printf("%d is positive\n", n);
