5ae7e8ccf6e8746b124a5bd6bd69144e86ef2fd1
Gary Masinga <76396923+Garycoco@users.nonreply.github.com>
2022-03-12 00:57:39 +0000

    Project

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
- * main - Entry point
+ * main - Prints all possible combinations of two digit numbers ranging from 0 to 99
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -9,7 +9,7 @@ int main(void)
 
 	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (num2 = num1 + 1; num2 <= 98; num2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
