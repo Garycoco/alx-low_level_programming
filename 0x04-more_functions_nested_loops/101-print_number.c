7531ab0dd394b34c8f3a4babaeb6eca31876294f
Gary Masinga <76396923+Garycoco@users.nonreply.github.com>
2022-03-17 18:31:15 +0000

    Tast 101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ *
+ * Return: void
+ *
+ * @n: integer parameter
+ */
+
+void print_number(int n)
+{
+	unsigned int doubles, single, i = n;
+	double j = 1;
+
+	if (n == 0)
+	_putchar('0');
+	else
+	{
+		if (n < 0)
+		{
+			i = n * -1;
+			_putchar('-');
+		}
+		while (j <= i)
+		j *= 10;
+		doubles = j / 10;
+
+		while (doubles >= 1)
+		{
+			single = i doubles;
+			_putchar(single + 0);
+			i = (i - (doubles * single));
+			doubles /= 10;
+		}
+	}
+}
