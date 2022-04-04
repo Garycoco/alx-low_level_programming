721b180b15daa8b5a623df25b69b7ee97d2d0879
Gary Masinga <76396923+Garycoco@users.nonreply.github.com>
2022-04-04 12:12:57 +0000

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,19 +16,20 @@ char *_strdup(char *str)
 	char *duplicate_str;
 	int i = 0, len = 0;
 
-	if (str == NULL) /* validate str input */
-		return (NULL);
-		while (*(str + i))
-			len++, i++;
-			len++; /* add null terminator to length */
-			duplicate_str = malloc(sizeof(char) * len); /* allocate memory */
-			if (duplicate_str == NULL) /* validate memory */
-				return (NULL);
-				i = 0;
-				while (i < len)
-				{
-					*(duplicate_str + i) = *(str + i);
-					i++;
-				}
-				return (duplicate_str);
+	if (str == NULL)
+	return (NULL);
+	while (*(str + 1))
+	len++, i++;
+	len++;
+	duplicate_str = malloc(sizeof(char) * len);
+
+	if (duplicate_str == NULL)
+	return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		*(duplicate_str + i) = *(str + i);
+		i++;
+	}
+	return (duplicate_str);
 }
