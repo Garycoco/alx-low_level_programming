ee243ca0025c9daad63a1457f0559ff2fc5c0896
Gary Masinga <76396923+Garycoco@users.nonreply.github.com>
2022-04-04 11:51:17 +0000

    free grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -15,9 +15,9 @@ void free_grid(int **grid, int height)
 	int i;
 
 	if (grid == NULL)
-		free(grid);
+	free(grid);
 
-		for (i = 0; i < height; i++)
-			free(grid[i]);
-			free(grid);
+	for (i = 0; i < height; i++)
+	free(grid[i]);
+	free(grid);
 }
