d37859abc60a2e18ac377536526c2e16f929b350
Gary Masinga <76396923+Garycoco@users.nonreply.github.com>
2022-04-14 22:33:00 +0000

    Tasks

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include "variadic_functions.h"
 
 /**
  * print_strings - prints strings
@@ -20,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(va, char*);
+		str = va_arg(va, char *);
 		if (str == NULL)
 			printf("(nil)");
 		else
