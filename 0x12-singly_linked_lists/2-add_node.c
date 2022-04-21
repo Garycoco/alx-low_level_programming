e85776f598257610365ceb199eee5441706afe62
Gary Masinga <76396923+Garycoco@users.nonreply.github.com>
2022-04-21 18:10:42 +0000

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,38 +1,40 @@
 #include "lists.h"
 #include <string.h>
+
 /**
- *add_node - function to add item at the beginning of a linked_list
- *@head: head pointer to linked_list
- *@str: string to add
+ * add_node - Adds a new node at the beggining of a list
  *
- *Return: address of new element
+ * @head: head pointer to a linked list
+ * @str: string to add
+ *
+ * Return: Adress of new element
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new_node;
-unsigned int i = 0;
+	list_t *new_node;
+	unsigned int i = 0;
 
-if (str == NULL)
-return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-new_node = malloc(sizeof(list_t));
-if (new_node == NULL)
-return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-new_node->str = strdup(str);
-if (new_node->str == NULL)
-{
-free(new_node);
-return (NULL);
-}
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-while (new_node->str[i])
-{
-i++;
-}
-new_node->len = i;
-new_node->next = *head;
-*head = new_node;
+	while (new_node->str[i])
+	{
+		i++;
+	}
+	new_node->len = i;
+	new_node->next = *head;
 
-return (new_node);
+	return (new_node);
 }
